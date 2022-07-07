19fbed11211b0a975ef5bfb86781d15c5c74fdcd
JOJO_HOME <iamjojo1225@gmail.com>
2022-07-07 22:01:04 +0800

    no message

--- app.js
+++ app.js
@@ -12,44 +12,7 @@ const config = {
 const channelSecret = config.channelSecret;
 // 創建 LINE SDK 客戶端
 const client = new line.Client(config);
-// create Express app
-// about Express itself: <https://expressjs.com/>
 const app = express();
-
-
-
-app.post('/test', (req, res) => {
-    console.log('run 0-1 req: ', req)
-    console.log('run 0-2 res: ', res)
-    // 給 LINE 的 body 要是 string
-    const body = JSON.stringify(req.body);
-    console.log('run 1: ', body);
-
-    // 取得 LINE 的簽名
-    // const signature = crypto.createHmac('SHA256', channelSecret).update(body).digest('base64');
-    // 取得 headers 中的 X-Line-Signature
-    // const headerX = req.get('x-line-signature');
-
-    // 當LINE的簽名 與 X-Line-Signature 一致時
-    // console.log('run 2: ', signature, headerX);
-    // if (signature === headerX) {
-
-    // webhook event
-    const event = req.body.events[0];
-    console.log('run 3: ', event);
-
-
-
-    // Promise
-    //     .all(req.body.events.map(handleEvent))
-    //     .then((result) => res.json(result))
-    //     .catch((err) => {
-    //         console.error(err);
-    //         res.status(500).end();
-    //     });
-});
-
-
 app.post('/linewebhook', line.middleware(config), (req, res) => {
     // 給 LINE 的 body 要是 string
     const body = JSON.stringify(req.body);
